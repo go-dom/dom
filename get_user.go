bd5b4b312f955fd89efc0f57f765b4467b9cbcce
Zero <54469706+3JoB@users.noreply.github.com>
2023-04-02 13:42:00 +0800

    fix bugs

--- get_user.go
+++ get_user.go
@@ -6,7 +6,7 @@ import (
 
 func GetUser(seed, blockhash string, userNum, prizeNum int) []int64 {
 	bigSeed, _ := new(big.Int).SetString(seed, 16)
-	winner := make([]int64, 0, prizeNum)
+	var winner []int64
 	for i := 0; i < prizeNum; i++ {
 		winnerID := bigSeed.Mod(bigSeed, big.NewInt(int64(userNum))).Int64()
 		if winnerID != 0 {
